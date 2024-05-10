9be79f463c17b62d5018ef4fa89d002b8e85eb22
brjatoba92 <brjatoba92@gmail.com>
2024-05-10 17:01:12 -0300

    Create Check-In Table

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -12,7 +12,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
            params: z.object({
             attendeeId: z.coerce.number().int(),
            }),
-           response: {},
+           response: {
+            200: z.object({
+                badge: z.object({
+                    name: z.string(),
+                    email: z.string().email(),
+                    eventTitle: z.string(),
+                    checkInURL: z.string().url(),
+                })
+            })
+           },
         }
       }, async (request, reply) => {
         const { attendeeId } = request.params
@@ -36,6 +45,18 @@ export async function getAttendeeBadge(app: FastifyInstance) {
             throw new Error('Attendee not found. ')
         }
 
-        return reply.send({ attendee })
+        const baseURL = `${request.protocol}://${request.hostname}`
+
+        console.log(baseURL)       
+
+        const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL) 
+
+        return reply.send({ 
+            badge: {
+               name: attendee.name, 
+               email: attendee.email,
+               eventTitle: attendee.event.title,
+               checkInURL: checkInURL.toString(),
+            } })
       })
 }

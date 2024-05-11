159debcb363f30cb70399fbeb61e1ec8852b2a60
brjatoba92 <brjatoba92@gmail.com>
2024-05-11 11:48:36 -0300

    Error Handler

---
+++ src/error_handler.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from "fastify"
+import { BadRequest } from "./routes/_errors/bad-request"
+
+type FastifyErrorHandler = FastifyInstance['errorHandler']
+
+export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
+    if (error instanceof BadRequest) {
+        return reply.status(400).send({ message: error.message })
+    }
+
+    return reply.status(500).send({ message: 'Um erro aconteceu' })
+}
